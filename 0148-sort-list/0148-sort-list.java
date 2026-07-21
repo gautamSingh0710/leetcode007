5d04764b34aae052bd0c9378395a18b4b100d4fc
Gautam Singh <gagangau@gmail.com>
2026-07-21 22:08:40 +0530

    Time: 9 ms (98.9%), Space: 59.5 MB (37.25%) - LeetHub

--- 0148-sort-list/0148-sort-list.java
+++ 0148-sort-list/0148-sort-list.java
@@ -1,54 +1,57 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-
     public ListNode sortList(ListNode head) {
-
-        if(head==null || head.next==null)
+        
+        if(head==null || head.next==null){
             return head;
-
+        }
         ListNode slow=head;
         ListNode fast=head.next;
-
         while(fast!=null && fast.next!=null){
             slow=slow.next;
             fast=fast.next.next;
-        }
 
+        }
         ListNode right=slow.next;
         slow.next=null;
 
-        ListNode left=sortList(head);
         right=sortList(right);
-
-        return merge(left,right);
+        ListNode left=sortList(head);
+    return merge(left,right);
     }
-
-    private ListNode merge(ListNode a,ListNode b){
-
-        ListNode dummy=new ListNode(-1);
-        ListNode tail=dummy;
-
+     public ListNode merge(ListNode a,ListNode b){
+        ListNode current=new ListNode(-1);
+        ListNode dummy=current;
         while(a!=null && b!=null){
-
             if(a.val<b.val){
-
-                tail.next=a;
-                a=a.next;
+             current.next=a;
+              a = a.next;
             }
             else{
-
-                tail.next=b;
-                b=b.next;
+                current.next=b;
+                b = b.next;
             }
-
-            tail=tail.next;
+            current=current.next;
         }
+        if(a!=null){
+            current.next=a;
 
-        if(a!=null)
-            tail.next=a;
+        }
+        else{
+            current.next=b;
+        }
+     
 
-        if(b!=null)
-            tail.next=b;
 
-        return dummy.next;
+      return dummy.next;  
     }
 }
