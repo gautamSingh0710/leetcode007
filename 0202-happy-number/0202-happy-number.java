7cf1366c33a5b80365057bfd80b645fc06971009
Gautam Singh <gagangau@gmail.com>
2026-07-23 21:00:39 +0530

    Time: 1 ms (77.61%), Space: 42.5 MB (51.31%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.java
@@ -0,0 +1,26 @@
+import java.util.HashSet;
+
+class Solution {
+    public boolean isHappy(int n) {
+
+        HashSet<Integer> set = new HashSet<>();
+
+        while (n != 1) {
+            if (set.contains(n)) {
+                return false;
+            }
+            set.add(n);
+            int sum = 0;
+            int temp = n;
+            while (temp > 0) {
+                int digit = temp % 10;
+                sum += digit * digit;
+                temp /= 10;
+            }
+
+            n = sum;
+        }
+
+        return true;
+    }
+}
