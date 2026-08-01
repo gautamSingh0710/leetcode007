9b0d9e8fdfbfa7c550b28de214f7ab9ab004083c
Gautam Singh <gagangau@gmail.com>
2026-08-02 00:33:47 +0530

    Time: 0 ms (100%), Space: 42.4 MB (94.79%) - LeetHub

--- 0486-predict-the-winner/0486-predict-the-winner.java
+++ 0486-predict-the-winner/0486-predict-the-winner.java
@@ -1,17 +1,28 @@
 class Solution {
-      public int helper(int[] nums, int left, int right) {
+
+    public boolean predictTheWinner(int[] nums) {
+        int n = nums.length;
+        Integer[][] dp = new Integer[n][n];
+
+        return helper(nums, 0, n - 1, dp) >= 0;
+    }
+
+    public int helper(int[] nums, int left, int right, Integer[][] dp) {
 
         if (left == right) {
             return nums[left];
         }
 
-        int takeLeft = nums[left] - helper(nums, left + 1, right);
+        if (dp[left][right] != null) {
+            return dp[left][right];
+        }
 
-        int takeRight = nums[right] - helper(nums, left, right - 1);
+        int takeLeft = nums[left] - helper(nums, left + 1, right, dp);
 
-        return Math.max(takeLeft, takeRight);
-    }
-    public boolean predictTheWinner(int[] nums) {
-        return helper(nums, 0, nums.length - 1) >= 0;
+        int takeRight = nums[right] - helper(nums, left, right - 1, dp);
+
+        dp[left][right] = Math.max(takeLeft, takeRight);
+
+        return dp[left][right];
     }
 }
