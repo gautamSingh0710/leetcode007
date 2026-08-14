0aa64bd7c4facb3893f83a96f2251efbc98b6baa
Gautam Singh <gagangau@gmail.com>
2026-08-14 12:11:20 +0530

    Time: 5 ms (54.13%), Space: 48.8 MB (10.72%) - LeetHub

--- 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.java
+++ 2816-double-a-number-represented-as-a-linked-list/2816-double-a-number-represented-as-a-linked-list.java
@@ -1,40 +1,42 @@
-import java.math.BigInteger;
-
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-
 class Solution {
     public ListNode doubleIt(ListNode head) {
 
-        StringBuilder sb = new StringBuilder();
-        ListNode temp = head;
+        head = reverse(head);
 
-        while (temp != null) {
-            sb.append(temp.val);
-            temp = temp.next;
-        }
+        ListNode curr = head;
+        int carry = 0;
 
-        BigInteger num = new BigInteger(sb.toString());
-        num = num.multiply(BigInteger.valueOf(2));
+        while (curr != null) {
 
-        String ans = num.toString();
+            int val = curr.val * 2 + carry;
 
-        ListNode dummy = new ListNode(0);
-        ListNode curr = dummy;
+            curr.val = val % 10;
+            carry = val / 10;
 
-        for (int i = 0; i < ans.length(); i++) {
-            curr.next = new ListNode(ans.charAt(i) - '0');
+            if (curr.next == null)
+                break;
+
             curr = curr.next;
         }
 
-        return dummy.next;
+        // Agar last mein carry bacha
+        if (carry > 0) {
+            curr.next = new ListNode(carry);
+        }
+
+        return reverse(head);
+    }
+
+    ListNode reverse(ListNode head) {
+        ListNode prev = null;
+
+        while (head != null) {
+            ListNode next = head.next;
+            head.next = prev;
+            prev = head;
+            head = next;
+        }
+
+        return prev;
     }
 }
